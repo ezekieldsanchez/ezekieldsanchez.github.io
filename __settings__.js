5f8e97dfe9e99d4743aaf634e54567dd0acb461e
Ezekiel <ezekieldsanchez@gmail.com>
2024-06-13 16:46:40 +0800

    trying 3D

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2017558.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 184302330, 184302331, 184302332, 184302333 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/184302354/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/184302355/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/184302353/1/ammo.js', 'preload' : true},
+];
